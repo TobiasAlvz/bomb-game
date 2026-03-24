4ad4f6a7f01d34bc5c093b18d2200f861d8ad7c4
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-24 16:20:40 -0300

    Update styles.js

--- pages/start/styles.js
+++ pages/start/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 import { RFValue } from "react-native-responsive-fontsize";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
@@ -6,4 +6,11 @@ export const Container = styled.View`
   flex: 1;
   padding-top: ${getStatusBarHeight() + RFValue(50)}px;
   background-color: #131313;
+
+`;
+
+export const Logo = styled.Image`
+  width: ${RFValue(270)}px;
+  height: ${RFValue(50)}px;
+  margin: 0 auto;
 `;
