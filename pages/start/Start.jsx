ecee596b8eb1ca9e7b7c105cb9de76f736a382bc
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-24 16:43:15 -0300

    Update Start.jsx

--- pages/start/Start.jsx
+++ pages/start/Start.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container } from "./styles";
 import { Logo } from "./styles";
+import { Title } from "./styles";
+import { SubTitle } from "./styles";
 
 export default function Start() {
   return (
@@ -9,6 +11,8 @@ export default function Start() {
         source={require("../../assets/logoDark.png")}
         style={{ resizeMode: "contain" }}
       />
+      <Title>Bem-vindo ao {"\n"} Bomb game</Title>
+      <SubTitle>Escolha um modo de jogo.</SubTitle>
     </Container>
   );
 }
