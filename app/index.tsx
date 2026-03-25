ac6d3a56f5855db6a7e9ca98d791c66890b656f7
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-25 16:16:58 -0300

    up

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,38 @@
 import Start from "../pages/start/Start";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { useCallback, useEffect } from "react";
+import { View } from "react-native";
 
 export default function App() {
-  return <Start />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+    <Start />
+  </View>;
 }
