4f7c6d2e18fdee2a903f69992d5b09fa3cf69fcd
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-25 14:33:26 -0300

    Update Start.jsx

--- pages/start/Start.jsx
+++ pages/start/Start.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, Rules } from "./styles";
 import { Logo } from "./styles";
 import { Title } from "./styles";
 import { SubTitle } from "./styles";
+import ButtonComponent from "../../components/buttons/index";
+import { Alert } from "react-native";
 
 export default function Start() {
+  function handleNavToPlayAlone() {
+    Alert.alert("Botão clicado 1");
+  }
+
+  function handleNavToPlayTogether() {
+    Alert.alert("Botão clicado 2");
+  }
+
+  function handleNavToRules() {
+    Alert.alert("Rules clicado!");
+  }
   return (
     <Container>
       <Logo
@@ -13,6 +26,15 @@ export default function Start() {
       />
       <Title>Bem-vindo ao {"\n"} Bomb game</Title>
       <SubTitle>Escolha um modo de jogo.</SubTitle>
+      <ButtonComponent
+        buttonText={"Jogar Solo"}
+        handlePress={handleNavToPlayAlone}
+      />
+      <ButtonComponent
+        buttonText={"Jogar Em Dupla"}
+        handlePress={handleNavToPlayTogether}
+      />
+      <Rules onPress={handleNavToRules}>Ver as regras do jogo</Rules>
     </Container>
   );
 }
