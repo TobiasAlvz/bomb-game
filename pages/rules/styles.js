43b66f30cb0d51bca92fd8df4c2fef113c3a82e5
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-25 16:23:54 -0300

    Update styles.js

--- pages/rules/styles.js
+++ pages/rules/styles.js
@@ -7,35 +7,26 @@ export const Container = styled.View`
   flex: 1;
   padding: 20px;
   padding-top: ${getStatusBarHeight() + RFValue(20)}px;
-  background-color: #131313;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Icon = styled(MaterialIcons)`
   font-size: ${RFValue(30)}px;
   margin-bottom: ${RFValue(20)}px;
-  color: white;
-`;
-
-export const ScrollTextRules = styled.ScrollView.attrs({
-  showsVerticalScrollIndicator: false,
-})`
+  color: ${({ theme }) => theme.colors.white};
 `;
 
 export const Title = styled.Text`
   font-size: ${RFValue(32)}px;
-  font-family: sans-serif;
+  font-family: ${({ theme }) => theme.fonts.bold};
   margin-left: ${RFValue(10)}px;
-  color: white;
+  color: ${({ theme }) => theme.colors.white};
 `;
 
 export const Paragraph = styled.Text`
   font-size: ${RFValue(16)}px;
-  font-family: sans-serif;
+  font-family: ${({ theme }) => theme.fonts.regular};
   margin-left: ${RFValue(10)}px;
   margin-top: ${RFValue(15)}px;
-  color: #bbb;
-`;
-
-export const NumberParagraph = styled.Text`
-  font-weight: bold;
+  color: ${({ theme }) => theme.colors.textLight};
 `;
