204d2f380bde814486701fc2a902c29440bea9ae
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-26 14:14:37 -0300

    Create index.jsx

---
+++ pages/PlayAlone/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+export default function PlayAlone() {
+  return (
+    <Container>
+      <Title>Bomb Game Solo</Title>
+    </Container>
+  );
+}
