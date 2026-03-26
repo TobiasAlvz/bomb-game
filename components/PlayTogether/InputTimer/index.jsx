1c2691a6b781e9dcdb62a5de154b62559d24a360
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-26 15:56:59 -0300

    Update index.jsx

--- components/PlayTogether/InputTimer/index.jsx
+++ components/PlayTogether/InputTimer/index.jsx
@@ -2,15 +2,21 @@ import { ImageBackground } from "expo-image";
 import bombImg from "../../../assets/bomba.png";
 import { Input, InputContainer, TextTimer, Timer } from "./style";
 import { useRef } from "react";
+import { Keyboard } from "react-native";
 
 export default function InputTimer() {
-  const input1 = useRef();
-  const input2 = useRef();
-  const input3 = useRef();
+  const input1 = useRef(null);
+  const input2 = useRef(null);
+  const input3 = useRef(null);
+
+  const hoursInput = (value) => {};
+  const minutesInput = (value) => {};
+  const secondsInput = (value) => {};
+
   return (
     <ImageBackground
       source={bombImg}
-      resizeMode="cover"
+      contentFit="cover"
       style={{
         marginTop: 50,
         minHeight: 130,
@@ -21,28 +27,47 @@ export default function InputTimer() {
       <Timer>
         <InputContainer>
           <Input
-            keyboardType={"number-pad"}
+            keyboardType="number-pad"
             maxLength={2}
             placeholder="00"
             placeholderTextColor="#bbb"
+            ref={input1}
+            onChangeText={(value) => {
+              if (value.length > 1) input2.current?.focus();
+              hoursInput(value);
+            }}
           />
         </InputContainer>
+
         <TextTimer>:</TextTimer>
+
         <InputContainer>
           <Input
-            keyboardType={"number-pad"}
+            keyboardType="number-pad"
             maxLength={2}
             placeholder="00"
             placeholderTextColor="#bbb"
+            ref={input2}
+            onChangeText={(value) => {
+              if (value.length > 1) input3.current?.focus();
+              minutesInput(value);
+            }}
           />
         </InputContainer>
+
         <TextTimer>:</TextTimer>
+
         <InputContainer>
           <Input
-            keyboardType={"number-pad"}
+            keyboardType="number-pad"
             maxLength={2}
             placeholder="00"
             placeholderTextColor="#bbb"
+            ref={input3}
+            onChangeText={(value) => {
+              if (value.length > 1) Keyboard.dismiss();
+              secondsInput(value);
+            }}
           />
         </InputContainer>
       </Timer>
