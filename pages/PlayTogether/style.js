8ceacbfda259a3ef61e8d93d2cc4becbb7176c36
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-26 15:49:12 -0300

    Create style.js

---
+++ pages/PlayTogether/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+export const Container = styled.View`
+  flex: 1;
+  padding-top: ${getStatusBarHeight() + RFValue(30)}px;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(32)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  text-align: center;
+  color: ${({ theme }) => theme.colors.white};
+`;
