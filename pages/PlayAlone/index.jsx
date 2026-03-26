ea7bc30124fe7e5005797ca151fe9969d90869f1
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-26 14:32:55 -0300

    Update index.jsx

--- pages/PlayAlone/index.jsx
+++ pages/PlayAlone/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { Container, TextTimer, Timer, Title } from "./styles";
+import { ImageBackground } from "expo-image";
+import bombImg from "../../assets/bomba.png";
 
 export default function PlayAlone() {
   return (
     <Container>
       <Title>Bomb Game Solo</Title>
+      <ImageBackground
+        source={bombImg}
+        resizeMode="cover"
+        style={{
+          marginTop: 50,
+          minHeight: 130,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Timer>
+          <TextTimer> {"00 : 05 : 00"}</TextTimer>
+        </Timer>
+      </ImageBackground>
     </Container>
   );
 }
