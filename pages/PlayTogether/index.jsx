bcc0cdd8ee22302613deffab7de11e59575c1511
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-26 16:23:14 -0300

    Update index.jsx

--- pages/PlayTogether/index.jsx
+++ pages/PlayTogether/index.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import { Container, Title } from "./style";
 import InputTimer from "../../components/PlayTogether/InputTimer";
 import TipInput from "../../components/PlayTogether/TipInput";
+import PasswordInput from "../../components/PasswordInput";
+import { router } from "expo-router";
+import { Alert, Button } from "react-native";
 
 export default function PlayTogether() {
+  function handleNavToStart() {
+    router.push("/");
+  }
+
+  function handleStartGame() {
+    Alert.alert("Jogo começou!");
+  }
   return (
     <Container>
       <Title>Bomb Game Dupla</Title>
       <InputTimer />
-      <BombMessage>Mensagem de erro temporária!</BombMessage>
+      {/* <BombMessage>Mensagem temporária de erro</BombMessage> */}
       <TipInput />
+      <PasswordInput />
+      <Button title="Iniciar" onPress={handleStartGame} />
+
+      <Button title="Página Inicial" onPress={handleNavToStart} />
     </Container>
   );
 }
