2f1da260987b051135de0c0217a65067aeea094a
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-26 15:49:10 -0300

    Create index.jsx

---
+++ pages/PlayTogether/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Title } from "./style";
+
+export default function PlayTogether() {
+  return (
+    <Container>
+      <Title>Bomb Game Dupla</Title>
+    </Container>
+  );
+}
