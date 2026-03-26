7859f969148c38f46671e346a719829b175d4799
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-26 16:15:34 -0300

    Update index.jsx

--- pages/PlayTogether/index.jsx
+++ pages/PlayTogether/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Container, Title } from "./style";
+import InputTimer from "../../components/PlayTogether/InputTimer";
+import TipInput from "../../components/PlayTogether/TipInput";
 
 export default function PlayTogether() {
   return (
     <Container>
       <Title>Bomb Game Dupla</Title>
+      <InputTimer />
+      <BombMessage>Mensagem de erro temporária!</BombMessage>
+      <TipInput />
     </Container>
   );
 }
