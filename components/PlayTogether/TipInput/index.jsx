6cd42db0af8e26ee075aba5a0be8f6b5d7fc730d
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-26 16:15:26 -0300

    Create index.jsx

---
+++ components/PlayTogether/TipInput/index.jsx
@@ -0,0 +1,13 @@
+import { Container, Input, InputContainer, TipTitle } from "./style";
+
+export default function TipInput() {
+ return (
+    <Container>
+      <TipTitle>Dica de senha:</TipTitle>
+        <InputContainer>
+          <Input
+            placeholder="Dica para a sua dupla"
+          />
+        </InputContainer>
+    </Container>
+  )};
