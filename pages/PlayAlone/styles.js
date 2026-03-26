5cc0e1f32f3c2621155fdf1db568eb11195efbb2
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-26 14:32:58 -0300

    Update styles.js

--- pages/PlayAlone/styles.js
+++ pages/PlayAlone/styles.js
@@ -14,3 +14,14 @@ export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.white};
   text-align: center;
 `;
+
+export const Timer = styled.View`
+  margin-bottom: 25px;
+  margin-right: 15px;
+`;
+
+export const TextTimer = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.white};
+`;
