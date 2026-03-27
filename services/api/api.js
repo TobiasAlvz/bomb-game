6f4f0360fa9e7d18491e5506f6c715737d3f838d
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-27 15:39:15 -0300

    Update api.js

--- services/api/api.js
+++ services/api/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create ({
-  baseURL: 'http://ipdapessoa:PortaDaEscolha',
+  baseURL: 'http://172.16.72.170:3000',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
