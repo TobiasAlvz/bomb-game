a1b1308cd9289901edf917aae2f4cf2c6a8b64b2
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-27 16:28:38 -0300

    Update index.jsx

--- pages/PlayAlone/index.jsx
+++ pages/PlayAlone/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import {
   Container,
   TextTimer,
@@ -7,21 +7,31 @@ import {
   TipText,
   TipTitle,
   Title,
-} from "./styles";
-import { ImageBackground } from "expo-image";
-import bombImg from "../../assets/bomba.png";
-import PasswordInput from "../../components/PasswordInput";
-import { Alert, Button } from "react-native";
-import { router } from "expo-router";
+} from './styles';
+import {ImageBackground} from 'expo-image';
+import bombImg from '../../assets/bomba.png';
+import PasswordInput from '../../components/PasswordInput';
+import {Alert, Button} from 'react-native';
+import {router} from 'expo-router';
 
-export default function PlayAlone() {
-  function handleNavToStart() {
-    router.push("/");
+export default function PlayAlone () {
+  function handleNavToStart () {
+    router.push ('/');
   }
 
-  function handleStartGame() {
-    Alert.alert("Jogo começou!");
+  function handleStartGame () {
+    Alert.alert ('Jogo começou!');
   }
+
+  function handleStartBomb() {
+    const diffTime = null
+  }
+
+  const [started, setStarted] = useState (false);
+  const [pin, setPin] = useState (['', '', '']);
+  const [hours, setHours] = useState ('00');
+  const [minutes, setMinutes] = useState ('03');
+  const [seconds, setSeconds] = useState ('00');
   return (
     <Container>
       <Title>Bomb Game Solo</Title>
@@ -31,12 +41,12 @@ export default function PlayAlone() {
         style={{
           marginTop: 50,
           minHeight: 130,
-          alignItems: "center",
-          justifyContent: "center",
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
         <Timer>
-          <TextTimer> {"00 : 05 : 00"}</TextTimer>
+          <TextTimer> {'00 : 05 : 00'}</TextTimer>
         </Timer>
       </ImageBackground>
       <TipContainer>
