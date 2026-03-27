eee5b199659712c67cf311be0b3ed9ca8b146ade
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-27 14:17:42 -0300

    Update index.jsx

--- pages/Disarmed/index.jsx
+++ pages/Disarmed/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Container, Logo} from './style';
+import {Container, Logo, SucessImg, Title} from './style';
 
 import logoImg from '../../assets/logoDark.png';
+import sucessImg from '../../assets/bomba_cortada_matrix.png';
+import {router} from 'expo-router';
+import {Button} from 'react-native';
 
 export default function Disarmed () {
+  function handleNavToStart () {
+    router.push ('/');
+  }
   return (
     <Container>
       <Logo source={logoImg} style={{resizeMode: 'contain'}} />
+      <Title>Parabéns!!!{'\n'}Você desarmou</Title>
+      <SucessImg source={sucessImg} style={{resizeMode: 'contain'}} />
+      <Button buttonText={'Página incial'} handlePress={handleNavToStart} />
     </Container>
   );
 }
