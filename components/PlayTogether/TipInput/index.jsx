fcae5cbc7b668d7e6613bd689bc27fc17186a1e1
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-30 16:24:00 -0300

    Update index.jsx

--- components/PlayTogether/TipInput/index.jsx
+++ components/PlayTogether/TipInput/index.jsx
@@ -1,13 +1,17 @@
 import { Container, Input, InputContainer, TipTitle } from "./style";
 
-export default function TipInput() {
- return (
+export default function TipInput({ started, question, setQuestion }) {
+  return (
     <Container>
       <TipTitle>Dica de senha:</TipTitle>
-        <InputContainer>
-          <Input
-            placeholder="Dica para a sua dupla"
-          />
-        </InputContainer>
+      <InputContainer>
+        <Input
+          value={question}
+          onChangeText={setQuestion}
+          editable={!started}
+          placeholder="Dica"
+        />
+      </InputContainer>
     </Container>
-  )};
+  );
+}
