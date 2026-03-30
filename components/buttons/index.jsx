9e27970c7024d446f1e9f971ebfb40b805c17644
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-30 16:23:58 -0300

    Update index.jsx

--- components/buttons/index.jsx
+++ components/buttons/index.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import {Button, ButtonContent} from './styles';
+import { Button, ButtonContent } from "./styles";
 
-export default function ButtonComponent({buttonText, handlePress}) {
+export default function ButtonComponent({ buttonText, handlePress }) {
   return (
-    <Button activeOpacity={0.95} onPress={handlePress}>
+    <Button onPress={handlePress}>
       <ButtonContent>{buttonText}</ButtonContent>
     </Button>
   );
