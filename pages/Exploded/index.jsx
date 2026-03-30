c1330f6a7a13d6b0406edcc459053267091ad299
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-30 15:11:01 -0300

    Update index.jsx

--- pages/Exploded/index.jsx
+++ pages/Exploded/index.jsx
@@ -1,24 +1,35 @@
 import {Container, FailedImg, Logo, Title} from './styles';
 import logoImg from '../../assets/logoDark.png';
 import failedImg from '../../assets/bomba_explodiu.png';
-import {Button, Vibration} from 'react-native';
+import {Vibration} from 'react-native';
 import {router} from 'expo-router';
+import {useEffect} from 'react';
+import Button from '../../components/Button';
 
 export default function Exploded () {
   function handleNavToStart () {
     router.push ('/');
   }
 
-  setTimeout (function () {
-    Vibration.vibrate (4 * 1000);
-  }, 500);
+  useEffect (() => {
+    const timeout = setTimeout (() => {
+      Vibration.vibrate (4000);
+    }, 500);
+
+    return () => clearTimeout (timeout);
+  }, []);
 
   return (
     <Container>
       <Logo source={logoImg} style={{resizeMode: 'contain'}} />
-      <Title>Você falhou, a {'\n'} bomba explodiu!</Title>
+
+      <Title>
+        Você falhou, a {'\n'} bomba explodiu!
+      </Title>
+
       <FailedImg source={failedImg} style={{resizeMode: 'contain'}} />
-      <Button buttonText={'Página incial'} handlePress={handleNavToStart} />
+
+      <Button buttonText="Página Inicial" handlePress={handleNavToStart} />
     </Container>
   );
 }
