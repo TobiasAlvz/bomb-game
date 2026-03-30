76cdfc7b7fbb2553d4fb625198565badbe62fdf9
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-30 14:56:26 -0300

    Update index.jsx

--- components/buttons/index.jsx
+++ components/buttons/index.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import { Button, ButtonContent } from "./styles";
-import { Text } from "react-native";
+import React from 'react';
+import {Button, ButtonContent} from './styles';
 
-export default function ButtonComponent({ buttonText, handlePress }) {
+export default function ButtonComponent({buttonText, handlePress}) {
   return (
     <Button activeOpacity={0.95} onPress={handlePress}>
-      <Text>
-        <ButtonContent>{buttonText}</ButtonContent>
-      </Text>
+      <ButtonContent>{buttonText}</ButtonContent>
     </Button>
   );
 }
