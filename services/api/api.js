1d9e668e31486867251700c5d14c3fd3f386c6eb
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-30 14:41:20 -0300

    Update api.js

--- services/api/api.js
+++ services/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create ({
-  baseURL: 'http://172.16.72.170:3000',
+  baseURL: 'http://172.16.73.17:3000',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
