395afdb87a9bdef47922496553450c7a300f7218
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-04-01 15:31:15 -0300

    Update index.jsx

--- components/PasswordInput/index.jsx
+++ components/PasswordInput/index.jsx
@@ -3,53 +3,53 @@ import {Keyboard} from 'react-native';
 import {Container, Input, InputContainer} from './style';
 
 export default function PasswordInput({pin, setPin}) {
-  const input1 = useRef ();
-  const input2 = useRef ();
-  const input3 = useRef ();
+  const input1 = useRef();
+  const input2 = useRef();
+  const input3 = useRef();
 
-  function handleChange (value, index) {
+  function handleChange(value, index) {
     const newPin = [...pin];
     newPin[index] = value;
-    setPin (newPin);
+    setPin(newPin);
   }
 
   return (
     <Container>
-      <InputContainer>
+      <InputContainer active={pin[0] !== ''}>
         <Input
           keyboardType="number-pad"
           maxLength={1}
           ref={input1}
           value={pin[0]}
           onChangeText={value => {
-            handleChange (value, 0);
-            if (value) input2.current.focus ();
+            handleChange(value, 0);
+            if (value) input2.current.focus();
           }}
         />
       </InputContainer>
 
-      <InputContainer>
+      <InputContainer active={pin[1] !== ''}>
         <Input
           keyboardType="number-pad"
           maxLength={1}
           ref={input2}
           value={pin[1]}
           onChangeText={value => {
-            handleChange (value, 1);
-            if (value) input3.current.focus ();
+            handleChange(value, 1);
+            if (value) input3.current.focus();
           }}
         />
       </InputContainer>
 
-      <InputContainer>
+      <InputContainer active={pin[2] !== ''}>
         <Input
           keyboardType="number-pad"
           maxLength={1}
           ref={input3}
           value={pin[2]}
           onChangeText={value => {
-            handleChange (value, 2);
-            if (value) Keyboard.dismiss ();
+            handleChange(value, 2);
+            if (value) Keyboard.dismiss();
           }}
         />
       </InputContainer>
