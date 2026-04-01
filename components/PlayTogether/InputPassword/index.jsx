2024f9a07a98fb64450192aba56905213273526b
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-04-01 16:01:56 -0300

    Update index.jsx

--- components/PlayTogether/InputPassword/index.jsx
+++ components/PlayTogether/InputPassword/index.jsx
@@ -1,16 +1,39 @@
-import { Container, Input, InputContainer} from "./style";
-export default function InputPassword() {
+import React from 'react';
+import { Container, Input, InputContainer } from './style';
+
+export default function PasswordInput({ pin, setPin }) {
+  const inputsRef = [];
+
+  const handleChange = (text, index) => {
+    const newPin = [...pin];
+    newPin[index] = text;
+    setPin(newPin);
+
+    if (text && index < 2) {
+      inputsRef[index + 1]?.focus();
+    }
+  };
+
+  const handleKeyPress = (e, index) => {
+    if (e.nativeEvent.key === 'Backspace' && !pin[index] && index > 0) {
+      inputsRef[index - 1]?.focus();
+    }
+  };
+
   return (
     <Container>
-      <InputContainer>
-        <Input keyboardType={"number-pad"} maxLength={1} />
-      </InputContainer>
-      <InputContainer>
-        <Input keyboardType={"number-pad"} maxLength={1} />
-      </InputContainer>
-      <InputContainer>
-        <Input keyboardType={"number-pad"} maxLength={1} />
-      </InputContainer>
+      {pin.map((value, index) => (
+        <InputContainer key={index}>
+          <Input
+            ref={ref => inputsRef[index] = ref}
+            keyboardType="number-pad"
+            maxLength={1}
+            value={value}
+            onChangeText={(text) => handleChange(text, index)}
+            onKeyPress={(e) => handleKeyPress(e, index)}
+          />
+        </InputContainer>
+      ))}
     </Container>
   );
 }
