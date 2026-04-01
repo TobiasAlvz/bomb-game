6ca4211bf88014270b6ccacd51073a7e82296394
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-04-01 16:02:18 -0300

    Update style.js

--- components/PlayTogether/InputTimer/style.js
+++ components/PlayTogether/InputTimer/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Timer = styled.View`
   flex-direction: row;
@@ -11,16 +12,21 @@ export const InputContainer = styled.View`
 `;
 
 export const Input = styled.TextInput`
-  background-color: #fff;
-  width: 60px;
-  height: 60px;
+  background-color: #1e293b;
+  width: 70px;
+  height: 70px;
   text-align: center;
-  font-size: 24px;
-  border-radius: 8px;
+  font-size: 28px;
+  border-radius: 16px;
+  color: #ff8c42;
+  font-weight: bold;
+  border-width: 2px;
+  border-color: #334155;
 `;
 
 export const TextTimer = styled.Text`
-  font-size: 32px;
-  color: #fff;
+  font-size: 40px;
+  color: #ff8c42;
   font-weight: bold;
+  margin: 0 5px;
 `;
