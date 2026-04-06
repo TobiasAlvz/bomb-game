e9b3060dd06dadcd78319589b560f99c7f341d2f
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-04-06 14:33:04 -0300

    Update styles.js

--- pages/rules/styles.js
+++ pages/rules/styles.js
@@ -1,43 +1,46 @@
-import styled from "styled-components/native";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
-import { RFValue } from "react-native-responsive-fontsize";
-import { MaterialIcons } from "@expo/vector-icons";
+import styled from 'styled-components/native';
+import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+import {RFValue} from 'react-native-responsive-fontsize';
+import {MaterialIcons} from '@expo/vector-icons';
+import {ScrollView} from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
   padding: 20px;
-  padding-top: ${getStatusBarHeight() + RFValue(20)}px;
-  background-color: ${({ theme }) => theme.colors.background};
+  padding-top: ${getStatusBarHeight () + RFValue (20)}px;
+  background-color: #0f0f1a;
 `;
 
-export const Icon = styled(MaterialIcons)`
-  font-size: ${RFValue(30)}px;
-  margin-bottom: ${RFValue(20)}px;
-  color: ${({ theme }) => theme.colors.white};
+export const Icon = styled (MaterialIcons)`
+  font-size: ${RFValue (28)}px;
+  margin-bottom: ${RFValue (20)}px;
+  color: #ff8c42;
 `;
 
-export const Title = styled.Text`
-  font-size: ${RFValue(32)}px;
-  font-family: ${({ theme }) => theme.fonts.bold};
-  margin-left: ${RFValue(10)}px;
-  color: ${({ theme }) => theme.colors.white};
-`;
-
-export const Paragraph = styled.Text`
-  font-size: ${RFValue(16)}px;
-  font-family: ${({ theme }) => theme.fonts.regular};
-  margin-left: ${RFValue(10)}px;
-  margin-top: ${RFValue(15)}px;
-  color: ${({ theme }) => theme.colors.textLight};
-`;
-
-import { ScrollView } from "react-native";
-
-export const ScrollTextRules = styled(ScrollView)`
+export const ScrollTextRules = styled (ScrollView)`
   flex: 1;
 `;
 
+export const Title = styled.Text`
+  font-size: ${RFValue (28)}px;
+  font-weight: bold;
+  margin-left: ${RFValue (10)}px;
+  margin-bottom: ${RFValue (10)}px;
+  color: #ffffff;
+`;
+
+export const Paragraph = styled.Text`
+  font-size: ${RFValue (15)}px;
+  font-weight: 400;
+  margin-left: ${RFValue (10)}px;
+  margin-top: ${RFValue (12)}px;
+  margin-bottom: ${RFValue (8)}px;
+  color: #94a3b8;
+  line-height: 24px;
+`;
+
 export const NumberParagraph = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.bold};
-  color: ${({ theme }) => theme.colors.white};
+  font-weight: bold;
+  color: #ff8c42;
+  font-size: ${RFValue (15)}px;
 `;
